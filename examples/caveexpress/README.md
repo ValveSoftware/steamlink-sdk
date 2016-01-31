7bdf383a4c3c3280071b182234bf6aa868326d6a
Sam Lantinga <slouken@libsdl.org>
2016-01-31 12:25:01 -0800

    Added the game CaveExpress to Steam Link examples, thanks to Martin Gerhardy

---
+++ examples/caveexpress/README.md
@@ -0,0 +1,4 @@
+
+This is an example of a complete SDL game on the Steam Link
+
+To build, run ./build_steamlink.sh
---
+++ examples/caveexpress/build_steamlink.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+
+TOP="${PWD}"
+SRC="${TOP}/caveexpress-src"
+
+#
+# Download the source to caveexpress
+#
+if [ ! -d "${SRC}" ]; then
+	git clone https://github.com/mgerhardy/caveexpress.git "${SRC}"
+fi
+
+#
+# Build it
+#
+source "${TOP}/../../setenv.sh"
+
+pushd "${SRC}"
+./contrib/scripts/steamlink.sh
+popd
+
+#
+# Install it
+#
+mkdir -p "${TOP}/steamlink/apps"
+cp -av cp-build-steamlink/steamlink/* "${TOP}/steamlink/apps"
+
+#
+# All done!
+#
+echo "Build complete!"
+echo
+echo "Put the steamlink folder onto a USB drive, insert it into your Steam Link, and cycle the power to install."
